b3d078fe85f6915d4bec6dc829bb5b1844a6c038
Sopanha Neth <sopanhaneth143@gmail.com>
2026-07-11 16:23:21 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const characters = [
+    "A","B","C","D","E","F","G","H","I","J","K","L","M",
+    "N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+    "a","b","c","d","e","f","g","h","i","j","k","l","m",
+    "n","o","p","q","r","s","t","u","v","w","x","y","z",
+    "0","1","2","3","4","5","6","7","8","9",
+    "~","`","!","@","#","$","%","^","&","*",
+    "(",")","-","_","+","=",
+    "{","}","[","]","|","\\",
+    ":",";","<",">",",",".","?","/"
+];
+
+let passwordOne = document.getElementById("passwordOne")
+let passwordTwo = document.getElementById("passwordTwo")
+let btn = document.getElementById("btn")
+
+btn.addEventListener("click", function() {
+    let randomPasswordOne ="";
+    let randomPasswordTwo ="";
+
+    for (let i = 0; i < 15; i ++) {
+            let randomIndex1 = Math.floor(Math.random() * characters.length);
+            randomPasswordOne += characters[randomIndex1];
+        }
+
+    for (let i = 0; i < 15; i ++) {
+            let randomIndex2 = Math.floor(Math.random() * characters.length)
+            randomPasswordTwo += characters[randomIndex2]
+        }
+    passwordOne.textContent = randomPasswordOne
+    passwordTwo.textContent = randomPasswordTwo
+})
+
+
+
